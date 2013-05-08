da1ee3a50546ba9477602c51f609c3efe8122167
Lauren <eastridgele@gmail.com>
2013-05-08 12:44:19 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pestr::Application.config.secret_token = 'a365c17abcaa499e66a9b31c0947e0c2fae04c3a5940b1cfef87aa919a781ce9ff4210c83e41ccddb0d19e7d8dbd29a6c347908bf108a1078cb8db12742bcb62'
