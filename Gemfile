880a6ace92832757feac9d75034426be4e50c639
Lauren <eastridgele@gmail.com>
2013-05-09 14:33:13 -0700

    add devise invitable

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.13'
 
 gem 'sqlite3'
 gem 'dynamic_form', '1.1.4'
+gem 'devise', '2.1.0'
+gem 'devise_invitable', '~> 1.1.0'
 
 
 # Gems used only for assets and not required
@@ -33,7 +35,6 @@ group :test do
   gem 'email_spec', '1.2.1'
 end
 
-gem 'devise', '2.1.0'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -11,4 +11,14 @@ feature "Signing in" do
     page.should have_content("Your account was successfully confirmed.")
     page.should have_content("Signed in as pestr@example.com")
   end
+
+  scenario "sign in via form" do
+    User.find_by_email("pestr@example.com").confirm!
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', with: "pestr@example.com"
+    fill_in 'Password', with: "password"
+    click_button "Sign in"
+    page.should have_content("Signed in successfully.")
+  end
 end
