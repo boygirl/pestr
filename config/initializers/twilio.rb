def840dd39a413e8e390f7587ef1834c87d688d7
Lauren <eastridgele@gmail.com>
2013-05-29 15:34:10 -0700

    add twilio initializer

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,6 @@
+#twilio credentials
+account_sid = 'AC4d1eba4a1c77f8275fcf87aa9e63cb0d'
+auth_token = '379998e86766546438c63ae5639358cd'
+TWILIO_NUMBER = '12064557201'
+TWILIO_CLIENT = Twilio::REST::Client.new account_sid, auth_token
+
