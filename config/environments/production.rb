2b48236185e20bd44bbf77147d514c1dd4af647b
Lauren <eastridgele@gmail.com>
2013-05-31 13:29:22 -0700

    add default url to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Pestr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  #send mail from deployed app
+  config.action_mailer.default_url_options = { host: 'pestr.us' }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
